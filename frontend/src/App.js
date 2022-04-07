2958eeb2181f2935e82327e09597f32168f6bd0f
amnw12 <48080226+amnw12@users.noreply.github.com>
2022-04-08 10:58:54 +1200

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,9 @@ import {
   BrowserRouter as Router
 } from "react-router-dom";
 
-
-//import Home from "./pages/Home";
-//import SEPractice from "./pages/SE-Practice";
-//import SubmitArticle from "./pages/Submit-Article"; 
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
 //import NotFoundPage from "./pages/404";
 
 const App = () =>  {
@@ -17,12 +16,14 @@ const App = () =>  {
         <div>
           <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
             <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
-            
+            <Route exact path="/" component={Home}/>
+            <Route  path="/SEPractice" component={SEPractice}/>
+            <Route  path="/SubmitArticle" component={SubmitArticle}/>
           </div>
         </div>
         </Router>
@@ -30,4 +31,4 @@ const App = () =>  {
 }
  
 export default App;
-
+ 
